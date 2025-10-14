1da4b38c01d8bb931c8c583ea9ed3e0c2ce94f41
AbdelrhmanHisham000 <aboodhisham222@gmail.com>
2025-10-14 15:26:21 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,9 +11,39 @@ const googleSans = Google_Sans_Code({
 });
 
 export const metadata = {
-  title: "Portfolio",
-  description: "Abdelrhman Hisham Portfolio",
+  title: 'Abdelrhman Hisham | Developer Portfolio',
+  description:
+    'Portfolio website of Abdelrhman Hisham: Web Developer, Designer, and Creator.',
+  openGraph: {
+    title: 'Abdelrhman Hisham | Developer Portfolio',
+    description:
+      'Portfolio website of Abdelrhman Hisham: Web Developer, Designer, and Creator.',
+    url: 'https://abdelrhmanhisham-portfolio.vercel.app/',
+    siteName: 'Abdelrhman Hisham',
+    images: [
+      {
+        url: 'https://abdelrhmanhisham-portfolio.vercel.app/img1.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Abdelrhman Hisham Portfolio Preview',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Abdelrhman Hisham | Developer Portfolio',
+    description:
+      'Portfolio website of Abdelrhman Hisham: Web Developer, Designer, and Creator.',
+    images: ['https://abdelrhmanhisham-portfolio.vercel.app/img1.jpg'],
+  },
+  icons: {
+    icon: '/favicon.ico',
+  },
+  manifest: '/manifest.json',
 };
+
 
 export default function RootLayout({ children }) {
   return (
